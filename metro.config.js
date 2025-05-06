3e7b6ab2c46386ef3c42e5bf389ab15d400b207f
sasanktumpati <sasanktumpati16@gmail.com>
2025-05-06 22:34:24 +0530

    [chore] adds generated icons

--- metro.config.js
+++ metro.config.js
@@ -7,4 +7,7 @@ const {
 
 const config = getDefaultConfig(__dirname);
 
+config.resolver.unstable_conditionNames = ["browser"];
+config.resolver.unstable_enablePackageExports = false;
+
 module.exports = withNativeWind(config, { input: "./src/global.css" });
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,26 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        herb: {
+          background: '#F5F6E7',
+          surface: '#F0F8EA',
+          accentSurface: '#C6CEC9',
+          primary: '#2B4D3F',
+          primaryDark: '#446835',
+          secondary: '#DAE5D8',
+          textPrimary: '#22332B',
+          textSecondary: '#4A5351',
+          divider: '#E5E6E4',
+          error: '#F28C0F',
+          card: '#FFFFFF',
+          success: '#008000',
+          muted: '#5F6F64',
+
+        }
+      }
+    },
   },
   future: {
     hoverOnlyWhenSupported: true,
